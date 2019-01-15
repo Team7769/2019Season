eb62eed458dda699d5f8012a9c3ad3da72bc044d
IMAGESOFT\cross <cross@imagesoftinc.com>
2019-01-14 19:33:47 -0500

    1-14 changes

---
+++ 2019Season/src/main/java/frc/robot/Configuration/Constants.java
@@ -0,0 +1,15 @@
+package frc.robot.Configuration;
+
+public final class Constants {
+
+    public final int kLFDriveId = 0;
+    public final int kLMDriveId = 1;
+    public final int kLRDriveId = 2;
+    public final int kRFDriveId = 3;
+    public final int kRMDriveId = 4;
+    public final int kRRDriveId = 5;
+
+    public final int kDriverControllerSlot = 0;
+    public final int kOperatorControllerSlot = 0;
+        
+}
---
+++ 2019Season/src/main/java/frc/robot/Utilities/RobotMap.java
@@ -0,0 +1,19 @@
+package frc.robot.Utilities;
+
+import com.revrobotics.CANSparkMax;
+import frc.robot.Configuration.*;
+
+public class RobotMap {
+
+    //6 Sparks
+    //6 TalonSRXs (2 Arm) (2 Ele) (2 Collector)
+    private CANSparkMax _lfDrive;
+    private CANSparkMax _lmDrive;
+    private CANSparkMax _lrDrive;
+    private CANSparkMax _rfDrive;
+    private CANSparkMax _rmDrive;
+    private CANSparkMax _rrDrive;
+
+    public RobotMap(){
+    }
+}
