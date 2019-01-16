420452e2fdfebf225467e1821cbce7bae0e9a7c1
IMAGESOFT\cross <cross@imagesoftinc.com>
2019-01-16 18:10:14 -0500

    Finished implementing RobotMap class.

--- 2019Season/src/main/java/frc/robot/Configuration/Constants.java
+++ 2019Season/src/main/java/frc/robot/Configuration/Constants.java
@@ -2,14 +2,22 @@ package frc.robot.Configuration;
 
 public final class Constants {
 
-    public final int kLFDriveId = 0;
-    public final int kLMDriveId = 1;
-    public final int kLRDriveId = 2;
-    public final int kRFDriveId = 3;
-    public final int kRMDriveId = 4;
-    public final int kRRDriveId = 5;
+    //Spark Device ID's
+    public static final int kLFDriveId = 0;
+    public static final int kLMDriveId = 1;
+    public static final int kLRDriveId = 2;
+    public static final int kRFDriveId = 3;
+    public static final int kRMDriveId = 4;
+    public static final int kRRDriveId = 5;
+    public static final int kLArmId = 6;
+    public static final int kRArmId = 7;
+    public static final int kLElevatorId = 8;
+    public static final int kRElevatorId = 9;
+    public static final int kLCollectorId = 10;
+    public static final int kRCollectorId = 11;
 
-    public final int kDriverControllerSlot = 0;
-    public final int kOperatorControllerSlot = 0;
+    //Controller Usb Slots
+    public static final int kDriverControllerSlot = 0;
+    public static final int kOperatorControllerSlot = 0;
         
 }
