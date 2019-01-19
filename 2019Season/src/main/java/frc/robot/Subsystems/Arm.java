43051062ac58ac6c08c4e56725e7a74fc6dd580a
IMAGESOFT\cross <cross@imagesoftinc.com>
2019-01-18 19:08:00 -0500

    Created Arm Subsystem.

---
+++ 2019Season/src/main/java/frc/robot/Subsystems/Arm.java
@@ -0,0 +1,31 @@
+package frc.robot.Subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Arm implements Subsystem{
+    private TalonSRX _leftTalon;
+    private TalonSRX _rightTalon;
+
+    public Arm(TalonSRX leftMotor, TalonSRX rightMotor){
+        _leftTalon = leftMotor;
+
+        _rightTalon = rightMotor;
+        _rightTalon.setInverted(true);
+    }
+
+    public void setSpeed(double speed) {
+        _leftTalon.set(ControlMode.PercentOutput, speed);
+        _rightTalon.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void WriteToDashboard() {
+        SmartDashboard.putNumber("leftArmSpeed", _leftTalon.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("leftArmPosition", _leftTalon.getSelectedSensorPosition());
+        SmartDashboard.putNumber("rightArmSpeed", _rightTalon.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("rightArmPosition", _rightTalon.getSelectedSensorPosition());
+    }
+
+}
